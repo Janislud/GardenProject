1f53a2d60d6cbfd4164346115619725d081cb66d
CherniyAleksandr <aleksandru.schwarz@gmail.com>
2024-02-08 19:53:57 +0200

    set Routing in App.jsx Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
+import { Route, Routes } from "react-router-dom";
 import { Header } from "./layout/Header/Header";
+import { NotFound } from "./Pages/NotFound";
+
 
 function App() {
   return (
     <>
       <div>
         <Header />
+        {/* <Routes>
+          <Route path='/main' element={MainPage} />
+            <Route path='/categories' element={Categories} />
+            <Route path='/all-products' element={AllProducts} />
+            <Route path='/all-sales' element={AllSales} />
+        </Routes> */}
       </div>
     </>
   );
