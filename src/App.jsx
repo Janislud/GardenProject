fe223b564cbdcf5de612d5b5170258122abeeeb7
CherniyAleksandr <aleksandru.schwarz@gmail.com>
2024-02-08 19:12:19 +0200

    Create Header and set Routing in Header, install local fonts

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import { Header } from "./layout/Header/Header";
 function App() {
   return (
     <>
-    <div>
-      <Header/>
-    </div>
+      <div>
+        <Header />
+      </div>
     </>
   );
 }
--- src/layout/Header/Header.jsx
+++ src/layout/Header/Header.jsx
@@ -1,6 +1,38 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import burgerMenu from "../../assets/images/HeaderMedia/burger-menu-right.svg";
+import basket from "../../assets/images/HeaderMedia/headerBag.svg";
+import logo from "../../assets/images/HeaderMedia/headerLogo.svg";
+import classes from "./Header.module.css";
 
 export const Header = () => {
     return (
-        <div>Privet Medved</div>
+        <div>
+            <header>
+                <img className={classes.logo} src={logo} alt="Logo" />
+                <nav className={classes.navMenu}>
+                    <ul className={classes.navList}>
+                        <li>
+                            <Link to='/main'>Main Page</Link>
+                        </li>
+                        <li>
+                            <Link to='/categories'>Categories</Link>
+                        </li>
+                        <li>
+                            <Link to='/all-products'>All products</Link>
+                        </li>
+                        <li>
+                            <Link to='/all-sales'>All sales</Link>
+                        </li>
+                    </ul>
+                </nav>
+                <div className={classes.basketWrapper}>
+                    <Link to="basket">
+                        <img src={basket} alt="Basket" />
+                    </Link>
+                    <img className={classes.burgerMenu} src={burgerMenu} alt="BurgerMenu" />
+                </div>
+            </header>
+        </div>
     )
 }
