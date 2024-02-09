f05fbd2fd04632b1653c1a731066bff3549e0830
Janislud <janisludborzs2@gmail.com>
2024-02-09 21:08:17 +0200

    create apiSlice end all get-request

--- src/Pages/MainPage.jsx
+++ src/Pages/MainPage.jsx
@@ -1,4 +1,28 @@
 
+import { useEffect } from 'react';
+import { useGetCategoriesByIdQuery } from '../slices/apiSlice';
+
+
 export const MainPage = () => {
-    return <div>MainPage</div>
+    
+const { data, error, isLoading } = useGetCategoriesByIdQuery(3);
+
+useEffect(() => {
+    if (error) {
+        console.log("Error fetchig data:" , error);
+    }
+},[error]);
+
+useEffect(() => {
+    if (isLoading) {
+        console.log("Loading");
+    } else if (data) {
+        console.log("Data:",data)
+    }
+},[isLoading,data,error])
+
+    
+    return <div>
+        1
+    </div>
 }
--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { dateApi } from "../slices/apiSlice";
 
 
 export const store = configureStore({
     reducer: {
-    }
+        [dateApi.reducerPath]: dateApi.reducer
+    },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(dateApi.middleware),
 })
 
