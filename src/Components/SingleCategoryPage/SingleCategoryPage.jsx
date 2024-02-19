e586bdbb27ed15ac5c29d3640f3b44b5a3f29481
Uliana B. <ulyana9963@gmail.com>
2024-02-19 14:15:41 +0100

    change tag&style in Footer & add component ProductsCard in Sales&SingleCategoryPage

--- src/Components/SingleCategoryPage/SingleCategoryPage.jsx
+++ src/Components/SingleCategoryPage/SingleCategoryPage.jsx
@@ -4,6 +4,7 @@ import { Link, useParams } from "react-router-dom";
 import { useGetCategoriesByIdQuery } from "../../slices/apiSlice";
 import { FilterBar } from "../FilterBar/FilterBar";
 import style from "./SingleCategoryPage.module.css";
+import { ProductsCard } from "../ProductsCard/ProductsCard";
 
 export const SingleCategoryPage = () => {
   const { id } = useParams();
@@ -55,37 +56,7 @@ export const SingleCategoryPage = () => {
       <FilterBar title="Annuals" />
       <div className={style.singleCategoryCardsWrapper}>
         {products.map((product) => (
-          <Link key={product.id} to={`/single-product/${product.id}`}>
-            <div className={style.singleCardWrapper} key={product.id}>
-              <div
-                className={
-                  style.sale +
-                  (product.discont_price ? " " + style.saleBox : "")
-                }
-              >
-                {product.price &&
-                  product.discont_price &&
-                  `-${Math.round(
-                    ((product.price - product.discont_price) / product.price) *
-                      100
-                  )}%`}
-              </div>
-              <img
-                className={style.singleCardImg}
-                src={`http://localhost:3333${product.image}`}
-                alt={product.title}
-              />
-              <div className={style.titlePriceWrapper}>
-                <div className={style.productTitle}>{product.title}</div>
-                <div className={style.priceBox}>
-                  <h2 className={style.newPrice}>${product.price}</h2>
-                  <p className={style.firstPrice}>
-                    {product.discont_price ? "$" + product.discont_price : null}
-                  </p>
-                </div>
-              </div>
-            </div>
-          </Link>
+          <ProductsCard key={product.id} product={product} />
         ))}
       </div>
     </section>
