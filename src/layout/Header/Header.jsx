b8b99542481257b1e58d65d8b69dbd0f5cc77593
CherniyAleksandr <aleksandru.schwarz@gmail.com>
2024-02-20 12:39:04 +0200

    burger menu fix, MainBanner button fix

--- src/layout/Header/Header.jsx
+++ src/layout/Header/Header.jsx
@@ -1,13 +1,14 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
-import burgerMenu from "../../assets/images/HeaderMedia/burger-menu-right.svg";
-import basket from "../../assets/images/HeaderMedia/headerBag.svg";
-import logo from "../../assets/images/HeaderMedia/headerLogo.svg";
-import styles from "./Header.module.css";
+import React, { useState } from 'react';
+import { Link, useLocation, } from 'react-router-dom';
+import basket from '../../assets/images/HeaderMedia/headerBag.svg';
+import logo from '../../assets/images/HeaderMedia/headerLogo.svg';
+import styles from './Header.module.css';
+
 
 export const Header = () => {
 
     const [isOpen, setIsOpen] = useState(false)
+    const location = useLocation()
 
     const toggleMenu = () => {
         setIsOpen(!isOpen)
@@ -15,31 +16,35 @@ export const Header = () => {
 
     return (
         <header className={styles.headerWrapper}>
-            <img className={styles.logo} src={logo} alt="Logo" />
-            <nav className={styles.navMenu}>
+            <img className={styles.logo} src={logo} alt='Logo' />
+            <nav className={styles.navMenu} onClick={toggleMenu}>
                 <ul className=
-                {`${styles.navList} ${isOpen ? styles.menuToggle : ''}`}>
+                    {`${styles.navList} ${isOpen ? styles.menuToggle : ''}`}>
                     <li className={styles.listStyle}>
-                        <Link to='/'>Main Page</Link>
+                        <Link to='/' className={location.pathname === '/' ? styles.active : ''}>Main Page</Link>
                     </li>
                     <li className={styles.listStyle}>
-                        <Link to='/categories'>Categories</Link>
+                        <Link to='/categories' className={location.pathname === '/categories' ? styles.active : ''}>Categories</Link>
                     </li>
                     <li className={styles.listStyle}>
-                        <Link to='/all-products'>All products</Link>
+                        <Link to='/all-products' className={location.pathname === '/all-products' ? styles.active : ''}>All products</Link>
                     </li>
                     <li className={styles.listStyle}>
-                        <Link to='/all-sales'>All sales</Link>
+                        <Link to='/all-sales' className={location.pathname === '/all-sales' ? styles.active : ''}>All sales</Link>
                     </li>
                 </ul>
             </nav>
             <div className={styles.basketWrapper}>
-                <Link to="/cart">
-                    <img src={basket} alt="Basket" />
+                <Link to='/cart'>
+                    <img src={basket} alt='Basket' />
                 </Link>
-                <img className={styles.burgerMenu} src={burgerMenu} alt="BurgerMenu" onClick={toggleMenu} />
-
+                <div onClick={toggleMenu} className={`${styles.burger} ${isOpen ? styles.burger_active : ''}`}>
+                    <span className={`${styles.burger_line} ${styles.burger_line_first}`}></span>
+                    <span className={`${styles.burger_line} ${styles.burger_line_second}`}></span>
+                    <span className={`${styles.burger_line} ${styles.burger_line_third}`}></span>
+                    <span className={`${styles.burger_line} ${styles.burger_line_fourth}`}></span>
+                </div>
             </div>
-        </header>
+        </header >
     )
 }
