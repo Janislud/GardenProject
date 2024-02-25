7177769a77c77012a0b06f84cb3bd196407c0930
CherniyAleksandr <aleksandru.schwarz@gmail.com>
2024-02-25 21:29:13 +0200

    dark-mode

--- src/Pages/CategoriesPage.jsx
+++ src/Pages/CategoriesPage.jsx
@@ -3,7 +3,6 @@ import { AllCategories } from "../Components/AllCategories/AllCategories"
 
 
 export const CategoriesPage = () => {
-
     return (
         <main>
             <AllCategories /></main>
