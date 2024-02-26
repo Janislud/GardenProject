e55a9bb90902fb7ea61bc526c3ce73d6b963d808
Janislud <janisludborzs2@gmail.com>
2024-02-27 01:15:17 +0200

    restor to my last commit

--- src/Components/BreadCrumbs/BreadCrumbs.jsx
+++ src/Components/BreadCrumbs/BreadCrumbs.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { useLocation, Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { setBreadcrumbs } from "../../slices/breadcrumbsSlice";
 import style from "./BreadCrumbs.module.css";
 
