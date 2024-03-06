aeb26510dce714657706079bf77637ec168b6b66
Uliana B. <ulyana9963@gmail.com>
2024-03-06 15:41:24 +0100

    add documentation for users

--- src/Pages/MainPage.jsx
+++ src/Pages/MainPage.jsx
@@ -1,6 +1,5 @@
 import { useDispatch, useSelector } from "react-redux";
 import style from "./MainPage.module.css";
-import { useEffect, useState } from "react";
 import { Categories } from "../Components/Categories/Categories";
 import { DataUserForm } from "../Components/DataUserForm/DataUserForm";
 import { MainBanner } from "../Components/MainBanner/MainBanner";
@@ -10,26 +9,8 @@ import { toggleTheme } from "../slices/themaSlice";
 export const MainPage = ({ toggleThemeHandler }) => {
   const theme = useSelector(state => state.theme.theme)
   const dispatch = useDispatch();
-  const [loading, setLoading] = useState(true); // Состояние загрузки
 
-  // Имитация задержки загрузки данных
-  useEffect(() => {
-    setTimeout(() => {
-      setLoading(false); // Установка состояния загрузки в false после задержки
-    }, 2000); // Имитация 2 секундной задержки загрузки данных
-  }, []);
 
-  // Если данные загружаются, отображаем анимацию загрузки
-  if (loading) {
-    return (
-      <div className={style.loader}>
-        <div className={style.loaderAnimation}></div>
-        <div className={style.loaderText}>Loading...</div>
-      </div>
-    );
-  }
-
-  // Если данные загружены, отображаем реальный контент
   return (
     <main className={`${theme === 'light' ? style.dark : style.light}`}>
       <MainBanner toggleThemeHandler={toggleThemeHandler} />
