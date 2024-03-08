346a43823e757a4c74dcd201f718eec45b1dc602
Uliana B. <ulyana9963@gmail.com>
2024-03-08 11:53:37 +0100

    edit DiscountModal

--- src/Components/DiscountModal/DiscountModal.jsx
+++ src/Components/DiscountModal/DiscountModal.jsx
@@ -30,12 +30,6 @@ export const DiscountModal = ({ isOpen, onRequestClose }) => {
     }
   }, [isOpen, products, isLoading, isError]);
 
-  // Убеждаемся, что функции обработчики вызываются с правильным продуктом
-  const handleAddToCart = () => {
-    if (selectedProduct) {
-      dispatch(addProductToCart({ ...selectedProduct, quantity: 1 }));
-    }
-  };
 
   return (
     <div className={`${style.modal} ${isActive ? style.active : ''}`} onClick={onRequestClose}>
