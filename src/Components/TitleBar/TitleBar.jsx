58c05af118ae64db0df16cc8824b5fab8b8bbe8f
Anna Tetjorin <bruno25-03@mail.de>
2024-03-11 23:10:48 +0100

    titlebar

--- src/Components/TitleBar/TitleBar.jsx
+++ src/Components/TitleBar/TitleBar.jsx
@@ -1,26 +1,40 @@
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import style from "./TitleBar.module.css";
-import { useDispatch, useSelector } from "react-redux";
-import { toggleTheme } from "../../slices/themaSlice";
 
 export const TitleBar = ({ title, linkTo, buttonText }) => {
-  const theme = useSelector(state => state.theme.theme)
+  const theme = useSelector((state) => state.theme.theme);
 
+  return (
+    <div
+      className={`${style.titleTextWrapper} ${
+        theme === "light" ? style.dark : style.light
+      }`}
+    >
+      <h2
+        className={`${style.titleDescription} ${
+          theme === "light" ? style.dark : style.light
+        }`}
+      >
+        {title}
+      </h2>
 
-return (
-  <div className={`${style.titleTextWrapper} ${theme === 'light' ? style.dark : style.light}`}>
-    <h2 className={`${style.titleDescription} ${theme === 'light' ? style.dark : style.light}`}>
-          {title}
-        </h2>
+      <div className={style.line}></div>
 
-        <div className={style.line}></div>
-
-    <button className={`${style.titleBtn} ${theme === 'light' ? style.dark : style.light}`}>
-      <Link className={`${style.titleBtnDescription} ${theme === 'light' ? style.dark : style.light}`} to={linkTo}>
-            {buttonText}
-          </Link>
-        </button>
-      </div>
-)
-}
-
+      <button
+        className={`${style.titleBtn} ${
+          theme === "light" ? style.dark : style.light
+        }`}
+      >
+        <Link
+          className={`${style.titleBtnDescription} ${
+            theme === "light" ? style.dark : style.light
+          }`}
+          to={linkTo}
+        >
+          {buttonText}
+        </Link>
+      </button>
+    </div>
+  );
+};
