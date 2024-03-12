38371b28eee6d5d1a34ffc730bb27224782a6603
Uliana B. <ulyana9963@gmail.com>
2024-03-12 13:18:47 +0100

    add theme to pagination & edit readme

--- src/UI/Pagination.jsx
+++ src/UI/Pagination.jsx
@@ -1,4 +1,5 @@
 import Pagination from "@mui/material/Pagination";
+import { useSelector } from "react-redux";
 
 const CustomPagination = ({ count, handleChange }) => {
   
@@ -7,24 +8,29 @@ const CustomPagination = ({ count, handleChange }) => {
     window.scrollTo(0, 0); // Прокрутка наверх страницы
   };
 
-  return (
-    <Pagination
-      count={count}
-      variant="outlined"
-      defaultPage={1}
-      color="secondary"
-      size="medium"
-      onChange={handlePageChange}
-      sx={{
-        maxWidth: "1300px",
-        display: "flex",
-        flexWrap: "wrap",
-        justifyContent: "center",
-        margin: "50px auto",
-        textAlign: "center",
-      }}
-    />
-  );
-};
+    const theme = useSelector((state) => state.theme.theme)
+  
+    return (
+      <Pagination
+        count={count}
+        variant="outlined"
+        defaultPage={1}
+        color={theme === 'light' ? "primary" : "secondary"}
+        size="medium"
+        onChange={handlePageChange}
+        sx={{
+          maxWidth: "1300px",
+          display: "flex",
+          flexWrap: "wrap",
+          justifyContent: "center",
+          margin: "50px auto",
+          textAlign: "center",
+          "& .MuiPaginationItem-root": {
+            color: theme === 'light' ? 'white' : 'black',
+          }
+        }}
+      />
+    );
+  };
 
 export default CustomPagination;
