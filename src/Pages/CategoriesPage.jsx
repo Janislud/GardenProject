b9772e4c3e3d4fe2d1e8b5c70517eef7e0d79520
Janis <janisludborzs2@gmail.com>
2024-04-01 21:33:09 +0300

    check

--- src/Pages/CategoriesPage.jsx
+++ src/Pages/CategoriesPage.jsx
@@ -3,6 +3,7 @@ import { AllCategories } from "../Components/AllCategories/AllCategories";
 export const CategoriesPage = () => {
   return (
     <main>
+      
       <AllCategories />
     </main>
   );
